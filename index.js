748113ecb170b7e6721cb6c87b9479e441fb289f
Anthony Ellis <amellis245@gmail.com>
2021-12-07 22:40:28 -0500

    created sql connection

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-require("dotenv").config();
+const { response } = require("express");
 const inquirer = require("inquirer");
 require("console.table");
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "codingcrazy",
+  database: "company_db",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connection listening");
+});
