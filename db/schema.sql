31fc2b2dc43b2eaee62e4f15b7a73dbfc2eb5a6d
Anthony Ellis <amellis245@gmail.com>
2021-12-15 21:33:29 -0500

    commented out managerid query in schema and seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,27 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
-
-USE company_db;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
 
 CREATE TABLE department (
-id INT PRIMARY KEY NOT NULL,
-name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT, 
+    name VARCHAR(30) NOT NULL, 
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-id INT PRIMARY KEY NOT NULL,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL,
-department_id INT
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(10.5) NOT NULL, 
+    department_id INT NOT NULL, 
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE employee (
-id INT PRIMARY KEY NOT NULL,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id INT,
-manager_id INT
+    id INT AUTO_INCREMENT, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT NOT NULL, 
+--     manager_id INT,
+    PRIMARY KEY(id)
+    
 );
-
-ALTER TABLE department CHANGE id id INT(8) AUTO_INCREMENT PRIMARY KEY(id);
-ALTER TABLE role CHANGE id id INT(8) AUTO_INCREMENT PRIMARY KEY(id);
-ALTER TABLE employee CHANGE id id INT(8) AUTO_INCREMENT PRIMARY KEY(id);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (name)
-VALUES ("Accounting"), ("Engineering"), ("HR"), ("Sales");
+VALUES ("Accounting"), ("Engineering"), ("HR"), ("Sales"),("Executive");
 
 INSERT INTO role (title, salary, department_id)
-VALUE ("Accountant", 65000.00, 1), ("Engineer", 85000, 2), ("Manager", 90000, 3), ("Sales Associate", 85650, 4);
+VALUE ("Accountant", 65000.00, 1), ("Engineer", 85000.00, 2), ("Manager", 90000.00, 3), ("Sales Associate", 85650.00, 4), ("CE0", 150000.00, 5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE ("Joe", "Shmo", 3, null), ("Tanner", "Banner", 2, 3), ("Shmandy", "Dandy", 3, 2), ("Mark", "Bark", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUE ("Joe", "Shmo", 4), ("Tanner", "Banner", 2), ("Shmandy", "Dandy", 1), ("Mark", "Bark", 5);
